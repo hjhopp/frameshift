232a991f938a9bbf12497c9fae318d6cffeb7850
Hann Jo Hopp <hann.jo.hopp@gmail.com>
2020-11-16 21:41:20 -0800

    stub out layout component

---
+++ src/components/layout/layout.svelte
@@ -0,0 +1,10 @@
+<script>
+import Gold from "../../../../qheroes/qheroes/home/components/gold/gold.svelte";
+import App from "../../../../svelte-app/src/App.svelte";
+
+</script>
+<link rel="stylesheet" href="./layout.css">
+
+<main class="{}">
+    <!-- layout shit here -->
+</main>
