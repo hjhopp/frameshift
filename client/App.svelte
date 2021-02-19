a1fea541af2a7ad790c86cd034739177a16c0a76
hjhopp <hann.jo.hopp@gmail.com>
2021-02-18 22:23:08 -0800

    pressing escape sends user back to home page

--- client/App.svelte
+++ client/App.svelte
@@ -1,6 +1,15 @@
 <script>
+    import { events } from "client/statechart/consts";
+    import { send }   from "client/stores/statechart";
+
     import Layout from "client/components/layout/layout.svelte";
 </script>
+
+<svelte:body on:keyup={({ code }) => {
+    if (code === "Escape") {
+        send(events.HOME);
+    }
+}} />
 
 <div data-testid="app">
     <Layout />
